16b4b6be46dafb34f09ea0b0279dcdc4ee322de0
TasneemAhmedEbied <ahmed.mohamed.ebied@gmail.com>
2025-05-08 19:49:08 +0300

    user dates (main)

---
+++ lib/taskprojectdates.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main() {
+  List<String> listDays = ["sat", "sun", "mon"];
+  List<int> bin = [];
+  List<Map<String, String>> system = [];
+  print("should use a day");
+  for (int i = 0; i < listDays.length; i++) {
+    print(" ${i + 1} - ${listDays[i]}");
+  }
+  for (int x = 0; x < listDays.length; x++) {
+    print("enter your name :");
+    String name = stdin.readLineSync()!;
+    print("choose day :");
+    int choose = int.parse(stdin.readLineSync()!);
+    if(choose > listDays.length){
+      print("should choose choice excit");
+      exit(0);
+    }
+    if (bin.contains(choose)) {
+      print("this day other one choose ");
+      exit(0);
+    } else {
+      system.add({"name": name, "day": listDays[choose - 1]});
+      bin.add(choose);
+    }
+  }
+  for (var systemName in system) {
+    print(systemName);
+  }
+}
